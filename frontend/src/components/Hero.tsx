8639b60b15dbe4a90ee4c1b4398443ccbfc324b9
lancebenedictos <lancebenedictos@gmail.com>
2024-03-29 20:41:00 -0400

    auth added

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,7 +1,9 @@
+import hero from "../assets/hero.png";
+
 const Hero = () => {
   return (
     <div>
-      <img />
+      <img src={hero} className="w-full max-h-[600px] object-cover" />
     </div>
   );
 };
