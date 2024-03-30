ee159cb5b015d73d2dfc4d5adece6317f27916d9
lancebenedictos <lancebenedictos@gmail.com>
2024-03-29 22:09:44 -0400

    mongodb user created with auth0

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
-
+import userRoutes from "./routes/UserRoutes";
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
   .then(() => console.log("connected to DB"));
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", async (req: Request, res: Response) => {});
+app.use("/api/my/user", userRoutes);
 
 app.listen(7000, () => {
   console.log("Success");
