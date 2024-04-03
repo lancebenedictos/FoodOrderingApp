40c5c8bd90a60c469a4d63cbb6734d9123d9ace1
lancebenedictos <lancebenedictos@gmail.com>
2024-04-03 17:38:29 -0400

    index update

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,20 +2,18 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import myUserRoute from "./routes/UserRoutes";
 import { v2 as cloudinary } from "cloudinary";
-var bodyParser = require("body-parser");
-
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
-import userRoutes from "./routes/UserRoutes";
 import restaurantRoute from "./routes/RestaurantRoute";
 import orderRoute from "./routes/OrderRoute";
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
-  .then(() => console.log("connected to DB"));
+  .then(() => console.log("Connected to database!"));
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
@@ -27,17 +25,16 @@ app.use(cors());
 app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.get("/health", async (req: Request, res: Response) => {
-  res.send({ message: "Health ok" });
+  res.send({ message: "health OK!" });
 });
 
-app.use("/api/my/user", userRoutes);
+app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", myRestaurantRoute);
 app.use("/api/restaurant", restaurantRoute);
 app.use("/api/order", orderRoute);
 
 app.listen(7000, () => {
-  console.log("Success");
+  console.log("server started on localhost:7000");
 });
